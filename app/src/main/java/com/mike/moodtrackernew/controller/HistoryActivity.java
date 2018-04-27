2803d0577950c6b59ac3d8a214ebda0155074e24
Mike <nottamike92@gmail.com>
2018-04-27 19:17:32 +0200

    add a field to get the current day

--- app/src/main/java/com/mike/moodtrackernew/controller/HistoryActivity.java
+++ app/src/main/java/com/mike/moodtrackernew/controller/HistoryActivity.java
@@ -6,10 +6,17 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
 import com.mike.moodtrackernew.R;
 import com.mike.moodtrackernew.model.AppDataBase;
 import com.mike.moodtrackernew.model.MoodData;
 import com.mike.moodtrackernew.view.RecyclerViewAdapter;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Calendar;
 import java.util.List;
 
 public class HistoryActivity extends AppCompatActivity {
@@ -25,13 +32,21 @@ public class HistoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.history_activity_layout);
 
+        Calendar c = Calendar.getInstance();
+        DateFormat df = new SimpleDateFormat("dd");
+        int mCurrentDay = Integer.parseInt(df.format(c.getTime()));
+        int mCurrentDay1 = mCurrentDay +6;
+
         //Passer en background thread
         AppDataBase db = Room.databaseBuilder(getApplicationContext(),
                 AppDataBase.class, "production")
                 .allowMainThreadQueries()
                 .build();
-        mMoodDataList = db.mMoodDataDAO().getAllMoodData();
+        mMoodDataList = Arrays.asList(db.mMoodDataDAO().loadAllMoodFromYesterday(mCurrentDay, mCurrentDay1));
 //        mMoodDataList.add(new MoodData(mCurrentDay, "TestComment", 123456));
+        for (MoodData str : mMoodDataList){
+            Log.d("BDD", "  " + str.getId() );
+        }
 
 
         mRecyclerView = findViewById(R.id.recyclerView);
