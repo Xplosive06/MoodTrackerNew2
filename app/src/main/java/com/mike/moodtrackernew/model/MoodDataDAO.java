86a9128c3d8275d52575cc1c72a9af982c1bd323
Mike <nottamike92@gmail.com>
2018-04-27 19:20:51 +0200

    add method to get information from production

--- app/src/main/java/com/mike/moodtrackernew/model/MoodDataDAO.java
+++ app/src/main/java/com/mike/moodtrackernew/model/MoodDataDAO.java
@@ -2,14 +2,19 @@ package com.mike.moodtrackernew.model;
 
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
 
 import java.util.List;
 @Dao
 public interface MoodDataDAO {
+
+    @Query("SELECT * FROM mooddata WHERE id BETWEEN :currentDay AND :currentDay1")
+    MoodData[] loadAllMoodFromYesterday(int currentDay, int currentDay1);
+
     @Query("SELECT * FROM mooddata")
     List<MoodData> getAllMoodData();
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insertAll(MoodData... moodData);
 }
